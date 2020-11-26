e55c80d73bd1bb22ed9a28d526c6dd8f16ee4175
Ewan Brain <ewan.brain@gmail.com>
2020-11-26 15:23:07 +0000

    mvp done

--- tests/task_decider_test.py
+++ tests/task_decider_test.py
@@ -12,14 +12,24 @@ class TestTaskDecider(unittest.TestCase):
         get_preferred_option(self.clean_windows, self.wash_dishes)
         self.assertEqual("clean_windows", (self.clean_windows.description))
 
-    def test_clean_windows_does_not_beat_wash_dishes(self):
+    def test_clean_windows_loses_to_wash_dishes(self):
         get_preferred_option(self.clean_windows, self.wash_dishes)
-        self.assertEqual("clean_windows", (self.clean_windows.description))
+        self.assertEqual("wash_dishes", (self.wash_dishes.description))
 
     def test_wash_dishes_beats_cook_dinner(self):
         get_preferred_option(self.wash_dishes, self.cook_dinner)
         self.assertEqual("wash_dishes", (self.wash_dishes.description))
+    
+    def test_wash_dishes_loses_to_clean_windows(self):
+        get_preferred_option(self.wash_dishes, self.clean_windows)
+        self.assertEqual("clean_windows", (self.clean_windows.description))
 
     def test_cook_dinner_beats_clean_windows(self):
         get_preferred_option(self.cook_dinner, self.clean_windows)
         self.assertEqual("cook_dinner", (self.cook_dinner.description))
+
+    def test_cook_dinner_loses_to_wash_dishes(self):
+        get_preferred_option(self.cook_dinner, self.wash_dishes)
+        self.assertEqual("wash_dishes", (self.wash_dishes.description))
+
+    
